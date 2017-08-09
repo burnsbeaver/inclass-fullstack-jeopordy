6296363467aa7a3f35630c3af856beab2e8c1eae
burnsbeaver <burnsbeaver@gmail.com>
2017-08-09 10:49:29 -0400

    Basic app is working! With server and mongoose

---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//mongoose connections
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI);
+const connection = mongoose.connection;
+
+connection.on('connected', () => {
+  console.log('Mongoose Connected')
+})
+
+connection.on('error', (err) => {
+  console.log('mongoose connection error' + err)
+})
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+});
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('App is listening on: ' + PORT );
+});
